575adf9c766e960d25ddb76d1e1d2d58a7046c9e
Meynard Mangabang <meynardmangabang@gmail.com>
2023-11-23 15:08:03 +0800

    Updated downloads/pdfsdir creation to recursive

--- src/MessageHandler/NewEntityPdfHandler.php
+++ src/MessageHandler/NewEntityPdfHandler.php
@@ -57,9 +57,10 @@ final class NewEntityPdfHandler
         // Set paper size
         $dompdf->setPaper('A4');
 
-        // Created pdf download dir if it does not exist
+        // Create pdf download dir if it does not exist
         if (!is_dir($this->parameterBag->get('pdf_directory'))) {
-            mkdir($this->parameterBag->get('pdf_directory'));
+            // Recursive dir creation
+            mkdir($this->parameterBag->get('pdf_directory'), 0777, true);
         }
 
         // Save to PDF file
